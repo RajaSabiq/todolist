f0cc30fc9f63f9ad5bfc16e660bc33c0594fde4e
RajaSabiq <ther.sabiqarfan@gmail.com>
2023-08-06 20:57:07 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todoList, setTodoList] = useState(['HELLO']);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>TODO LIST</h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          let todoValue = e.target['todo'].value;
+          let tempTodoList = todoList;
+          tempTodoList.push(todoValue);
+          setTodoList(tempTodoList);
+        }}
+      >
+        <input
+          type='text'
+          name='todo'
+          id='todo'
+          required
+          placeholder='Write something'
+        />
+        <button type='submit'>Submit</button>
+      </form>
+      <div className='listView'>
+        {todoList.map((value, index) => {
+          return <ul key={index}>{value}</ul>;
+        })}
+      </div>
     </div>
   );
 }
